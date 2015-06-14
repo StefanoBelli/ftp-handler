54efdb9fcb90ed9fb5ca00f88b1da4cad4cd84e0
StefanoBelli <sa.belli@hotmail.it>
2015-06-14 23:45:06 +0200

    install.sh : small install script, already ch-modded to exec

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+checkForOld()
+{
+	[[ -f /usr/bin/ftph ]] &&
+		printf "\033[33m*\033[0m ftph already present.... Overwriting\n" ||
+		echo ""
+}
+
+if [[ $USER == "root" ]]; then
+	checkForOld
+	printf "\033[32m*\033[0m Copying...\n"
+	cp src/ftp-handler.py /usr/bin/ftph 
+	printf "\033[32m*\033[0m Changing execution mode...\n"
+	chmod +x /usr/bin/ftph && 
+	printf "\033[32m*\033[0m Done.\nNow just execute ftph :)\n"
+	exit 0 ||
+	printf "\033[31m*\033[0m There was a problem.\n"
+	exit 1
+else
+	printf "\033[31m*\033[0m You need to be root, nothing to do.\n"
+fi
